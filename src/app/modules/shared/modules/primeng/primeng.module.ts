d8bd547b02d1ab3eb500ff0151c5103cfc870344
jeyofdev <jgregoire.dev@gmail.com>
2024-06-17 23:22:50 +0200

    style : form add activity

--- src/app/modules/shared/modules/primeng/primeng.module.ts
+++ src/app/modules/shared/modules/primeng/primeng.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { MenubarModule } from 'primeng/menubar';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  exports: [ButtonModule, MenubarModule, CardModule],
+  exports: [ButtonModule, MenubarModule, CardModule, InputTextModule],
 })
 export class PrimengModule {}
