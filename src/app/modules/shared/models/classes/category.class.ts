fa7bd75c2916afbfd1c77f330718f231f45bcb8c
jeyofdev <jgregoire.dev@gmail.com>
2024-06-17 23:06:58 +0200

    feat : get products by category

--- src/app/modules/shared/models/classes/category.class.ts
+++ src/app/modules/shared/models/classes/category.class.ts
@@ -1,11 +1,15 @@
+import { Product } from './product.class';
+
 export class Category {
   id: number;
   name: string;
   imgUrl: string;
+  products: Product[];
 
-  constructor(id: number, name: string, imgUrl: string) {
+  constructor(id: number, name: string, imgUrl: string, products: Product[]) {
     this.id = id;
     this.name = name;
     this.imgUrl = imgUrl;
+    this.products = products;
   }
 }
